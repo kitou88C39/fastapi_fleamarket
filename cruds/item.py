abd7c3f511aac7ffd26b773ea5dccdaf693c7a0b
kitoumasaomi <kanfutrooper@gmail.com>
2025-10-13 19:51:46 +0900

    cruds/item.pyを作成する

--- cruds/item.py
+++ cruds/item.py
@@ -1,5 +1,16 @@
 from enum import Enum
 from typing import Optional
 
+
+class ItemStatus(Enum):
+    ON_SALE = "on_sale"
+    SOLD_OUT = "sold_out"
 class Item:
-    def __init__(self, id: int, name: str, price: int, description: Optional[str], status: enum):
+    def __init__(
+        self, 
+        id: int, 
+        name: str, 
+        price: int, 
+        description: Optional[str], 
+        status: ItemStatus
+    ):
