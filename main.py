3cf80429361f1050f95c223280c615617500b8e1
kitoumasaomi <kanfutrooper@gmail.com>
2025-10-14 20:09:02 +0900

    main.pyを作成する

--- main.py
+++ main.py
@@ -5,26 +5,3 @@ from routers import item
 app = FastAPI()
 app.include_router(item.router)
 
-@app.get("/items")
-async def find_all():
-    return item_cruds.find_all()
-
-@app.get("/items/{id}")
-async def find_by_id(id: int):
-    return item_cruds.find_by_id(id)
-
-@app.get("/items/")
-async def find_by_name(name: str):
-    return item_cruds.find_by_name(name)
-
-@app.post("/items")
-async def create(item_create=Body()):
-    return item_cruds.create_item(item_create)
-
-@app.put("/items/{id}")
-async def update(id: int, item_update=Body()):
-    return item_cruds.update(id, item_update)
-
-@app.delete("/items/{id}")
-async def delete(id: int):
-    return item_cruds.delete(id)
