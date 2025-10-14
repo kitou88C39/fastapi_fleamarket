b813b707dea804997aa2e15f717bc76bcdc3e9c6
kitoumasaomi <kanfutrooper@gmail.com>
2025-10-14 20:11:16 +0900

    routers/item.pyを作成する

--- routers/item.py
+++ routers/item.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 from cruds import item as item_cruds
 
 router = APIRouter()
@@ -7,26 +7,26 @@ router = APIRouter()
 async def find_all():
     return item_cruds.find_all()
 
-@app.get("/items")
+@router.get("/items")
 async def find_all():
     return item_cruds.find_all()
 
-@app.get("/items/{id}")
+@router.get("/items/{id}")
 async def find_by_id(id: int):
     return item_cruds.find_by_id(id)
 
-@app.get("/items/")
+@router.get("/items/")
 async def find_by_name(name: str):
     return item_cruds.find_by_name(name)
 
-@app.post("/items")
+@router.post("/items")
 async def create(item_create=Body()):
     return item_cruds.create_item(item_create)
 
-@app.put("/items/{id}")
+@router.put("/items/{id}")
 async def update(id: int, item_update=Body()):
     return item_cruds.update(id, item_update)
 
-@app.delete("/items/{id}")
+@router.delete("/items/{id}")
 async def delete(id: int):
     return item_cruds.delete(id)
