2a404b831ee0890544a2c8f320bef30f025b3a7a
kitoumasaomi <kanfutrooper@gmail.com>
2025-10-15 19:50:34 +0900

    schemas.pyを作成する

--- schemas.py
+++ schemas.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Optional
 from pydantic import BaseModel, Field
 
@@ -5,3 +6,9 @@ class ItemCreate(BaseModel):
     name: str = Field(min_length=2, max_length=20, examples=["PC"])
     price: int = Field(gt=0, examples=[10000])
     description: Optional[str] = Field(None, examples=["備品です"])
+
+class ItemUpdate(BaseModel):
+    name: Optional[str] = Field(None, min_length=2, max_length=20, examples=["PC"])    
+    price: Optional[int] = Field(None, gt=0, examples=[10000])
+    description: Optional[str] = Field(None, examples=["使備品です"])
+    status: Optional[str] = Field(None, examples=["on_sale"])
