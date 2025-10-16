0cc461a8eb419495f53e61ae696601dc51f1c5ff
kitoumasaomi <kanfutrooper@gmail.com>
2025-10-16 20:18:07 +0900

    routers/item.pyを作成する

--- routers/item.py
+++ routers/item.py
@@ -1,4 +1,3 @@
-from typing import Optional
 from fastapi import APIRouter, Path, Query, HTTPException
 from cruds import item as item_cruds
 from schemas import ItemCreate, ItemUpdate, ItemResponse
@@ -24,13 +23,16 @@ async def find_by_name(name: str = Query(min_length=2, max_length=20)):
 async def create(item_create: ItemCreate):
     return item_cruds.create_item(item_create)
 
-@router.put("/{id}", response_model=Optional[ItemResponse])
+@router.put("/{id}", response_model=ItemResponse)
 async def update(item_update: ItemUpdate, id: int = Path(gt=0)):
     update_item = item_cruds.update(id, item_update)
     if not update_item:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return item_cruds.update(id, item_update)
+        raise HTTPException(status_code=404, detail="Item not updated")
+    return update_item
 
-@router.delete("/{id}", response_model=Optional[ItemResponse])
+@router.delete("/{id}", response_model=ItemResponse)
 async def delete(id: int = Path(gt=0)):
-    return item_cruds.delete(id)
+    delete_item = item_cruds.delete(id)
+    if not delete_item:
+        raise HTTPException(status_code=404, detail="Item not deleted")
+    return delete_item
