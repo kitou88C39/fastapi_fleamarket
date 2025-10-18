c7a737ca80dd600886067af5e75ce27633ee2b71
kitoumasaomi <kanfutrooper@gmail.com>
2025-10-18 14:50:23 +0900

    routers/item.pyを作成する

--- routers/item.py
+++ routers/item.py
@@ -11,35 +11,35 @@ DbDependency = Annotated[Session, Depends(get_db)]
 
 router = APIRouter(prefix="/items", tags=["Items"])
 
-@router.get("", response_model=list[ItemResponse])
-async def find_all():
-    return item_cruds.find_all()
+# @router.get("", response_model=list[ItemResponse])
+# async def find_all():
+#     return item_cruds.find_all()
 
-@router.get("/{id}", response_model=ItemResponse, status_code=status.HTTP_200_OK)
-async def find_by_id(id: int=Path(gt=0)):
-    found_item = item_cruds.find_by_id(id)
-    if not found_item:
-        raise HTTPException(status_code=404, detail="Item not found")
-    return found_item
+# @router.get("/{id}", response_model=ItemResponse, status_code=status.HTTP_200_OK)
+# async def find_by_id(id: int=Path(gt=0)):
+#     found_item = item_cruds.find_by_id(id)
+#     if not found_item:
+#         raise HTTPException(status_code=404, detail="Item not found")
+#     return found_item
 
-@router.get("/", response_model=list[ItemResponse], status_code=status.HTTP_200_OK)
-async def find_by_name(name: str = Query(min_length=2, max_length=20)):
-    return item_cruds.find_by_name(name)
+# @router.get("/", response_model=list[ItemResponse], status_code=status.HTTP_200_OK)
+# async def find_by_name(name: str = Query(min_length=2, max_length=20)):
+#     return item_cruds.find_by_name(name)
 
 @router.post("", response_model=ItemResponse, status_code=status.HTTP_201_CREATED)
 async def create(item_create: ItemCreate):
     return item_cruds.create_item(item_create)
 
-@router.put("/{id}", response_model=ItemResponse, status_code=status.HTTP_200_OK)
-async def update(item_update: ItemUpdate, id: int = Path(gt=0)):
-    update_item = item_cruds.update(id, item_update)
-    if not update_item:
-        raise HTTPException(status_code=404, detail="Item not updated")
-    return update_item
+# @router.put("/{id}", response_model=ItemResponse, status_code=status.HTTP_200_OK)
+# async def update(item_update: ItemUpdate, id: int = Path(gt=0)):
+#     update_item = item_cruds.update(id, item_update)
+#     if not update_item:
+#         raise HTTPException(status_code=404, detail="Item not updated")
+#     return update_item
 
-@router.delete("/{id}", response_model=ItemResponse, status_code=status.HTTP_200_OK)
-async def delete(id: int = Path(gt=0)):
-    delete_item = item_cruds.delete(id)
-    if not delete_item:
-        raise HTTPException(status_code=404, detail="Item not deleted")
-    return delete_item
+# @router.delete("/{id}", response_model=ItemResponse, status_code=status.HTTP_200_OK)
+# async def delete(id: int = Path(gt=0)):
+#     delete_item = item_cruds.delete(id)
+#     if not delete_item:
+#         raise HTTPException(status_code=404, detail="Item not deleted")
+#     return delete_item
