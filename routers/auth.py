6e13dc0603047f6b1388adf31aedaf374f1f6a9e
kitoumasaomi <kanfutrooper@gmail.com>
2025-10-20 13:22:33 +0900

    routers/auth.pyを作成する

--- routers/auth.py
+++ routers/auth.py
@@ -1,5 +1,6 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from starlette import status
 from cruds import auth as auth_cruds
@@ -10,7 +11,20 @@ from database import get_db
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
 DbDependency = Annotated[Session, Depends(get_db)]
+DbDependency = Annotated[OAuth2PasswordRequestForm, Depends()]
 
-@router.post("/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/signup", response_model=UserResponse, status_code=status.HTTP_201_CREATED
+)
 async def create_user(db: DbDependency, user_create: UserCreate):
     return auth_cruds.create_user(db, user_create)
+
+@router.post(
+    "/login", status_code=status.HTTP_200_OK
+)
+async def login(db: DbDependency, from_data: FormDependency):
+    user = auth_cruds.authenticate_user(db, from_data.username, from_data.password)
+    if not user:
+        raise HTTPException(status_code=404, detail="Incorrect username or password")
+    
+    return "Successful Authentication"
