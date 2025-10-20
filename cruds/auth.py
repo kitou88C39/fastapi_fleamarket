e55c368981d67b6a6abc088455bc8de43c7cc879
kitoumasaomi <kanfutrooper@gmail.com>
2025-10-20 09:03:48 +0900

    cruds/auth.pyを作成する

--- cruds/auth.py
+++ cruds/auth.py
@@ -22,11 +22,15 @@ def create(db: Session, user_create: UserCreate):
 
     return new_user
 
-    def authenticate(db: Session, username: str, password: str):
-        user = db.query(User).filter(User.username == username).first()
-        if not user:
-            return None
+def authenticate(db: Session, username: str, password: str):
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        return None
 
-        hash_password = hashlib.pbkdf2_hmac(
-            "sha256", user_create.password.encode(), salt, 1000
+    hash_password = hashlib.pbkdf2_hmac(
+        "sha256", password.encode(), user.salt, 1000
         ).hex()
+    if hash_password != user.password:
+        return None
+
+    return user
