7ec48075260f5d0f582d21a249499551f8be4848
kitoumasaomi <kanfutrooper@gmail.com>
2025-10-21 16:39:49 +0900

    test/conftest.pyを作成する

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,18 @@
 import os
 import sys
 app_dir = os.path.join(os.path.dirname(__file__), '..')
+sys.path.append(app_dir)
+
+import pytest
+from sqlalchemy import create_engine
+
+
+@pytest.fixture()
+def session_fixture():
+    engine = create_engine(
+        url="sqlite://",
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool
+
+        
+    )
