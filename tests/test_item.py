0a965f0d607ac3a817230b1b4fc13df8b1305ef1
kitoumasaomi <kanfutrooper@gmail.com>
2025-10-22 10:13:03 +0900

    test/test_item.pyを作成する

--- tests/test_item.py
+++ tests/test_item.py
@@ -4,18 +4,26 @@ from fastapi.testclient import TestClient
 def test_find_all(client_fixture: TestClient):
     response = client_fixture.get("/items")
     assert response.status_code == 200
-    items = response.json()
-    assert len(items) == 2
+    item = response.json()
+    assert len(item) == 2
 
 
 def test_find_by_id_SuccessCase(client_fixture: TestClient):
     response = client_fixture.get("/items/1")
     assert response.status_code == 200
-    items = response.json()
-    assert items["id"] == 1
+    item = response.json()
+    assert item["id"] == 1
 
 
 def test_find_by_id_ErrorCase(client_fixture: TestClient):
     response = client_fixture.get("/items/10")
     assert response.status_code == 404
-    items = response.json()["detail"] == "Item not found"
+    item = response.json()["detail"] == "Item not found"
+
+
+def test_find_by_id_name(client_fixture: TestClient):
+    response = client_fixture.get("/items/?name=PC1")
+    assert response.status_code == 200
+    item = response.json()
+    assert len(item) == 1
+    assert item[0]["name"] == "PC1"
