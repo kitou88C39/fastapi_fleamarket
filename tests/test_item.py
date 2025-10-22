a919173996312ca5b437afad4b05a27bce02eb70
kitoumasaomi <kanfutrooper@gmail.com>
2025-10-22 09:48:17 +0900

    test/test_item.pyを作成する

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,7 +1,15 @@
 from fastapi.testclient import TestClient
 
+
 def test_find_all(client_fixture: TestClient):
     response = client_fixture.get("/items")
     assert response.status_code == 200
     items = response.json()
     assert len(items) == 2
+
+
+def test_find_by_id_SuccessCase(client_fixture: TestClient):
+    response = client_fixture.get("/items/1")
+    assert response.status_code == 200
+    items = response.json()
+    assert items["id"] == 1
